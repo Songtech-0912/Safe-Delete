6aadf276b6cbc2fcefdec00b7b8f542aeeda7f41
Jacky Song <dixinlily@hotmail.com>
2021-09-26 14:40:49 -0400

    Simplified installation script and fixed its errors
    Former-commit-id: 05ca0d2ba446ca630a3653c51747d8da0b6852c5 [formerly d7a723566643c9b8d5ad0bfec13cb4409c57d994] [formerly 0b4d745c658c02fd44d7517e09b15541488cfbdf]
    Former-commit-id: 9e6b6773884f43588f7cea135fb5c7bb94a2a073
    Former-commit-id: e5e5aa19a2359e1de8b6a27295d752ce12b1dcf5

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "\e[1;34mThis script will install delete.py on your system, or update it if it is already installed\e[0m"
+echo -e "\e[1mThis script will install delete.py on your system, or update it if it is already installed\e[0m"
 read -p "Press ENTER to start: "
 echo
 echo -e "\e[1mThis script will require admin priviledges to install delete.py to \e[32m/usr/local/bin\e[0m"
@@ -10,14 +10,14 @@ case $input in
         curl https://raw.githubusercontent.com/Songtech-0912/Safe-Delete/master/delete.py > ~/delete
         chmod a+x ~/delete
         sudo mv ~/delete /usr/local/bin
-        echo -e "\e[1mWould you like to alias delete.py as 'rm'? This is recommended.\e[0m"
+        echo -e "\e[1m\nWould you like to alias delete.py as 'rm'? This is recommended.\e[0m"
         while true
         do
             read -r -p "Your choice [Y/N]: " input
             case $input in
                 [yY][eE][sS]|[yY])
-                    echo "export PATH=/usr/local/bin:$PATH" > ~/.profile
-                    echo "alias rm='delete'" > ~/.profile
+                    echo "export PATH=/usr/local/bin:$PATH" >> ~/.profile
+                    echo "alias rm='delete' # Added for delete.py" >> ~/.profile
                     echo "Alias created."
                     echo
                     echo -e "You can now run delete.py as \e[36mdelete\e[0m \e[32m<yourfile.txt>\e[0m"
@@ -28,7 +28,6 @@ case $input in
                     echo "Alias not created."
                     echo
                     echo -e "You can now run delete.py as \e[36mdelete\e[0m \e[32m<yourfile.txt>\e[0m"
-                    echo -e "You can also run delete.py as \e[36mrm\e[0m \e[32m<yourfile.txt>\e[0m"
                     break
                     ;;
                 *)
